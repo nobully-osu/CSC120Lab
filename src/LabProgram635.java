44f9a2f7ec3b7abe40e85f564a7067f9cfad9733
nobully_osu <matthew-313@live.com>
2025-10-02 16:02:20 -0500

    Add LabProgram635.java

--- src/LabProgram635.java
+++ src/LabProgram635.java
@@ -1,10 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LabProgram635 {
+    public static int[] reverseArray(int[] arr, int length){
+        int[] reversedArray = new int[length];
+
+        for (int i = length - 1; i >= 0; --i){
+            reversedArray[i] = arr[length - i - 1];
+        }
+        return reversedArray;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] userValues = new int[20];   // List of integers from input
 
+        int length = in.nextInt();
+        int[] userValues = new int[length];
 
+        for (int i = 0; i < length; ++i) {
+            userValues[i] = in.nextInt();
+        }
+
+        int[] userValuesReverse = reverseArray(userValues, length);
+
+        if (Arrays.equals(userValues, userValuesReverse)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
     }
 }
