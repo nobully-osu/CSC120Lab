6549d11f7a53ccd50b785643d08aecf4590ffd45
nobully_osu <matthew-313@live.com>
2025-10-15 13:04:24 -0500

    Update LabProgram722.java

--- src/Module7/LabProgram722.java
+++ src/Module7/LabProgram722.java
@@ -1,4 +1,22 @@
 package Module7;
 
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class LabProgram722 {
+    public static void main(String[] args) throws IOException {
+        Scanner scnr = new Scanner(System.in);
+
+        /* TODO: Declare any necessary variables here. */
+
+
+        /* TODO: Read a file name from the user and read the tsv file here. */
+
+
+        /* TODO: Compute student grades and exam averages, then output results to a text file here. */
+
+    }
 }
