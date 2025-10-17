35bec05a11f9c0514c7259ae46e1871e93e4fd95
nobully_osu <matthew-313@live.com>
2025-10-17 14:42:48 -0500

    Update LabProgram727.java

--- src/Module7/LabProgram727.java
+++ src/Module7/LabProgram727.java
@@ -1,4 +1,15 @@
 package Module7;
 
+import java.util.Scanner;
+
 public class LabProgram727 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String[] names = { "Ryley", "Edan", "Reagan", "Henry", "Caius", "Jane", "Guto", "Sonya", "Tyrese", "Johnny" };
+        int index;
+
+        index = scnr.nextInt();
+        /* Type your code here. */
+    }
 }
+
