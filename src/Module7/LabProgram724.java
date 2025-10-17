d6abe68347340e4c8e426967c15ffdb168abebea
nobully-osu <matthew-313@live.com>
2025-10-16 23:21:52 -0500

    Update LabProgram724.java

--- src/Module7/LabProgram724.java
+++ src/Module7/LabProgram724.java
@@ -1,4 +1,18 @@
 package Module7;
 
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class LabProgram724 {
+    public static void main(String[] args) throws IOException {
+        Scanner scnr = new Scanner(System.in);
+        int NUM_CHARACTERS = 26;      // Maximum number of letters
+        int MAX_WORDS = 10;           // Maximum number of synonyms per starting letter
+
+        String[][] synonyms = new String[NUM_CHARACTERS][MAX_WORDS];  // Declare 2D array for all synonyms
+        String[] words = new String[MAX_WORDS]; // The words of each input line
+
+        /* Type your code here. */
+    }
 }
